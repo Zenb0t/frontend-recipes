853719a0f3353d1fa6aa3a3ffda9ba0c526e8873
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-05-31 21:14:16 -0700

    debug buttons

--- src/app/MagicButton.tsx
+++ src/app/MagicButton.tsx
@@ -1,19 +1,27 @@
 import { Button } from "@mui/material";
 import { useAppDispatch } from "./hooks";
-import  genRecipe, { genChickenSoupRecipe }  from "../services/FakeData"
-import { addRecipe, fetchRecipes } from '../features/recipeBook/RecipeSlice';
+import genRecipe, { genChickenSoupRecipe } from "../services/FakeData"
+import { createRecipe, deleteAllRecipes, fetchRecipes } from '../features/recipeBook/RecipeSlice';
 
 export const MagicButton = () => {
 
     const dispatch = useAppDispatch();
 
-    const add = () => dispatch(addRecipe(genRecipe(1)));
-
+    const add = () => dispatch(createRecipe(genRecipe(1)));
+    const deleteAll = () => dispatch(deleteAllRecipes());
     const fetch = () => console.log(dispatch(fetchRecipes()));
 
-    const action = () => dispatch(addRecipe(genChickenSoupRecipe()));
+    const action = () => dispatch(createRecipe(genChickenSoupRecipe()));
     return (
-        <Button variant="contained" onClick={action}  onAuxClick={fetch}      >
-            Generate Recipe!
-        </Button>);
+        <>
+            <Button variant="contained" onClick={action} onAuxClick={add}      >
+                Generate Recipe!
+            </Button>
+            <Button variant="contained" onClick={fetch}   >
+                Fetch All
+            </Button>
+            <Button variant="contained" onClick={deleteAll}   >
+                Delete All
+            </Button>
+        </>);
 }

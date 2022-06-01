3a8a8189cf01f1ce554a4515975643212538732f
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-05-31 21:13:21 -0700

    feat: added CRUD thunks

--- src/features/recipeBook/RecipeSlice.tsx
+++ src/features/recipeBook/RecipeSlice.tsx
@@ -25,6 +25,7 @@ export const createRecipe = createAsyncThunk(
     async (recipe: RecipeModel, thunkAPI) => {
         try {
             const response = await recipesAPI.createRecipe(recipe);
+            console.log(`Create Recipe resp: ${response.data}`);
             return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error);
@@ -36,8 +37,44 @@ export const fetchRecipes = createAsyncThunk(
     'recipes/fetchRecipes',
     async () => {
         const response = await recipesAPI.getRecipes();
-        console.log(response);
+        console.log(`Fetch resp: ${response.data}`);
         return response.data;
+    }
+);
+
+export const updateRecipe = createAsyncThunk(
+    'recipeBook/updateRecipe',
+    async (recipe: RecipeModel, thunkAPI) => {
+        try {
+            const response = await recipesAPI.updateRecipe(recipe.id, recipe);
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error);
+        }
+    }
+);
+
+export const deleteRecipe = createAsyncThunk(
+    'recipeBook/deleteRecipe',
+    async (recipeId: string, thunkAPI) => {
+        try {
+            const response = await recipesAPI.deleteRecipe(recipeId);
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error);
+        }
+    }
+);
+
+export const deleteAllRecipes = createAsyncThunk(
+    'recipeBook/deleteAllRecipes',
+    async (thunkAPI) => {
+        try {
+            const response = await recipesAPI.deleteAllRecipes();
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
     }
 );
 
@@ -45,18 +82,6 @@ export const recipeSlice = createSlice({
     name: 'recipe',
     initialState,
     reducers: {
-        addRecipe: (state, action: PayloadAction<RecipeModel>) => {
-            state.recipeList.push(action.payload);
-        },
-        removeRecipe: (state, action: PayloadAction<RecipeModel>) => {
-            state.recipeList = state.recipeList.filter(recipe => recipe.id !== action.payload.id);
-        },
-        updateRecipe: (state, action: PayloadAction<RecipeModel>) => {
-            const recipe = state.recipeList.find(recipe => recipe.id === action.payload.id);
-            if (recipe) {
-                Object.assign(recipe, action.payload);
-            }
-        },
         toggleFavorite: (state, action: PayloadAction<RecipeModel>) => {
             const recipe = state.recipeList.find(recipe => recipe.id === action.payload.id);
             if (recipe) {
@@ -67,8 +92,52 @@ export const recipeSlice = createSlice({
     extraReducers: {
         [fetchRecipes.fulfilled.type]: (state, action: PayloadAction<RecipeModel[]>) => {
             state.recipeList = action.payload;
-        }
-    }
+        },
+        [fetchRecipes.pending.type]: (state, action: PayloadAction<RecipeModel[]>) => {
+            state.status = 'loading';
+        },
+        [fetchRecipes.rejected.type]: (state, action: PayloadAction<any>) => {
+            state.status = 'error';
+            state.error = action.payload;
+        },
+        [createRecipe.fulfilled.type]: (state, action: PayloadAction<RecipeModel>) => {
+            state.recipeList.push(action.payload);
+        },
+        [createRecipe.pending.type]: (state, action: PayloadAction<RecipeModel>) => {
+            state.status = 'loading';
+        },
+        [createRecipe.rejected.type]: (state, action: PayloadAction<any>) => {
+            state.status = 'error';
+            state.error = action.payload;
+        },
+        [updateRecipe.fulfilled.type]: (state, action: PayloadAction<RecipeModel>) => {
+            const index = state.recipeList.findIndex(recipe => recipe.id === action.payload.id);
+            state.recipeList[index] = {
+                ...state.recipeList[index],
+                ...action.payload
+            };
+        },
+        [updateRecipe.pending.type]: (state, action: PayloadAction<RecipeModel>) => {
+            state.status = 'loading';
+        },
+        [updateRecipe.rejected.type]: (state, action: PayloadAction<any>) => {
+            state.status = 'error';
+            state.error = action.payload;
+        },
+        [deleteRecipe.fulfilled.type]: (state, action: PayloadAction<RecipeModel>) => {
+            const recipeIndex = state.recipeList.findIndex(recipe => recipe.id === action.payload.id);
+            if (recipeIndex > -1) {
+                state.recipeList.splice(recipeIndex, 1);
+            }
+        },
+        [deleteRecipe.pending.type]: (state, action: PayloadAction<RecipeModel>) => {
+            state.status = 'loading';
+        },
+        [deleteRecipe.rejected.type]: (state, action: PayloadAction<any>) => {
+            state.status = 'error';
+            state.error = action.payload;
+        },
+    },
 });
 
 export const selectRecipes = (state: RootState) => state.recipeBook.recipeList;
@@ -77,6 +146,6 @@ export const selectFavoriteRecipes = (state: RootState) => state.recipeBook.reci
 
 export const selectRecipeById = (state: RootState, id: string) => state.recipeBook.recipeList.find(recipe => recipe.id === id);
 
-export const { addRecipe, removeRecipe, updateRecipe, toggleFavorite, } = recipeSlice.actions;
+export const { toggleFavorite, } = recipeSlice.actions;
 
 export default recipeSlice.reducer;

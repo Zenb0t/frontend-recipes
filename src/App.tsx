4bed3adfd93fca20002ebb69a6e645281eaab521
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-08-31 22:29:23 -0700

    refactored sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import './App.css';
-import MenuBar from './app/MenuBar';
-import { Box as MdBox } from '@mui/material';
-import drawerWidth from './app/MenuBar';
 import { Outlet } from 'react-router-dom';
 //Chakra imports
-import { Grid, GridItem } from '@chakra-ui/react';
+import { SidebarWithHeader } from './components/sidebar';
 
 
 /** The entry point to the main application */
 function App() {
 
   return (
-    <MdBox sx={{ display: 'flex' }}>
-      {/* <MenuBar /> */}
-      <MdBox component="main"
-        sx={{
-          marginTop: '56px',
-          flexGrow: 1, p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          justifyContent: 'center',
-        }}
-      >
-        <Outlet />
-      </MdBox>
-    </MdBox>
+    <SidebarWithHeader>
+      <Outlet />
+    </SidebarWithHeader>
   );
 }
 

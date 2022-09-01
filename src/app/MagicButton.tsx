5f65cca9373cd4aaa7f2b78f95226f73d6caf3c2
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-08-31 22:29:48 -0700

    refactor magic!

--- src/app/MagicButton.tsx
+++ src/app/MagicButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 import { useAppDispatch } from "./hooks";
 import genRecipe, { genChickenSoupRecipe } from "../services/FakeData"
 import { createRecipe, deleteAllRecipes, fetchRecipes } from '../features/recipeBook/RecipeSlice';
@@ -7,21 +7,32 @@ export const MagicButton = () => {
 
     const dispatch = useAppDispatch();
 
-    const add = () => dispatch(createRecipe(genRecipe(1)));
-    const deleteAll = () => dispatch(deleteAllRecipes());
-    const fetch = () => console.log(dispatch(fetchRecipes()));
+    function add() {
+        dispatch(createRecipe(genChickenSoupRecipe()));
+    }
+    function deleteAll() {
+        dispatch(deleteAllRecipes());
+    }
+    function fetch() {
+        dispatch(fetchRecipes());
+    }
+
 
     const action = () => dispatch(createRecipe(genChickenSoupRecipe()));
     return (
-        <>
-            <Button variant="contained" onClick={action} onAuxClick={add}      >
+        <ButtonGroup colorScheme="purple">
+            <Button variant="ghost" onClick={action} onAuxClick={add}      >
                 Generate Recipe!
             </Button>
-            <Button variant="contained" onClick={fetch}   >
+            <Button variant="ghost" onClick={fetch}   >
                 Fetch All
             </Button>
-            <Button variant="contained" onClick={deleteAll}   >
+            <Button variant="ghost" onClick={deleteAll}   >
                 Delete All
             </Button>
-        </>);
+        </ButtonGroup>);
 }
+
+
+
+

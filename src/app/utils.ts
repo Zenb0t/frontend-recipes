fd25a66e3f85fa45eefcd8b5491f17fda182608c
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-09-05 22:19:18 -0700

    added yup validation

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,5 +1,8 @@
+import { object, string, number, date, array } from 'yup';
 
+export const yup = { object, string, number, date, array };
 
 export function capitalizeFirstLetter(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+

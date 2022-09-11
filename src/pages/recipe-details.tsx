02851f0d32ef06a24b8f93a04b416bc542db67d5
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-09-10 23:18:41 -0700

    new recipe details page

---
+++ src/pages/recipe-details.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { IngredientTable } from "../components/ingredient-table";
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../app/hooks";
+import { selectRecipeById } from "../features/recipeBook/RecipeSlice";
+import { FavoriteButton } from "../components/favorite-button";
+
+
+export function RecipePage() {
+    let { recipeId } = useParams();
+    console.log(`recipeId: ${recipeId}`);
+
+    const recipe = useAppSelector((state) => selectRecipeById(state, recipeId!));
+
+    return (
+        <Box p={8} shadow="md" bg="whiteAlpha.900" borderRadius="lg">
+            <Heading>{recipe!.title}</Heading>
+            <Text>
+                Total time: {recipe!.totalTime.hours}h {recipe!.totalTime.minutes}min
+            </Text>
+            <Text>Cost: {recipe!.cost.toFixed(2)}</Text>
+            <FavoriteButton recipe={recipe!} />
+            <IngredientTable ingredients={recipe!.ingredients} />
+        </Box>
+    );
+}

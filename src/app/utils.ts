d4af96cb42a2d01f3998511220c1465cd359939d
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-09-10 23:19:13 -0700

    added Time class

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,8 +1,25 @@
 import { object, string, number, date, array } from 'yup';
 
+// yup validation schema
 export const yup = { object, string, number, date, array };
 
 export function capitalizeFirstLetter(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+//Wrapper
+export class Time {
+  hours: number;
+  minutes: number;
+
+  constructor(hours = 0, minutes = 0) {
+    this.hours = hours;
+    this.minutes = minutes;
+  }
+
+  toString() {
+    return `${this.hours}h ${this.minutes} min`;
+  }
 }
 

4677944bbad90fbe38138008c54cfb5b0e4d9293
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-10-26 22:46:45 -0700

    added image + ui fixes

--- src/pages/recipe-details.tsx
+++ src/pages/recipe-details.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 import { IngredientTable } from "../components/ingredient-table";
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../app/hooks";
@@ -7,20 +6,38 @@ import { selectRecipeById } from "../features/recipeBook/recipe-slice";
 import { FavoriteButton } from "../components/favorite-button";
 
 
-export function RecipePage() {
+export function RecipeDetailsPage() {
     let { recipeId } = useParams();
 
     const recipe = useAppSelector((state) => selectRecipeById(state, recipeId!));
 
     return (
-        <Box p={8} shadow="md" bg="whiteAlpha.900" borderRadius="lg">
-            <Heading>{recipe!.title}</Heading>
-            <Text>
-                Total time: {recipe!.totalTime.hours}h {recipe!.totalTime.minutes}min
-            </Text>
-            <Text>Cost: {recipe!.cost.toFixed(2)}</Text>
-            <FavoriteButton recipe={recipe!} />
-            <IngredientTable ingredients={recipe!.ingredients} />
-        </Box>
+        <Box
+            shadow="md"
+            bg={useColorModeValue("white", "gray.800")}
+            borderRadius="lg">
+            <Flex
+                borderRadius={["lg", "lg", "none", "none"]}
+                w={'full'}
+                h={'40vh'}
+                backgroundImage={
+                    recipe!.imageUrl
+                }
+                backgroundSize={'cover'}
+                backgroundPosition={'center center'}
+            >
+            </Flex>
+            <Box
+                p={8}>
+
+                <Heading>{recipe!.title}</Heading>
+                <Text>
+                    Total time: {recipe!.totalTime.hours}h {recipe!.totalTime.minutes}min
+                </Text>
+                <Text>Cost: {recipe!.cost.toFixed(2)}</Text>
+                <FavoriteButton recipe={recipe!} />
+                <IngredientTable ingredients={recipe!.ingredients} />
+            </Box>
+        </Box >
     );
 }

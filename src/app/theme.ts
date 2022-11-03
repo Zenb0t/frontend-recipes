b3236dec4c16db3cd47a8bc9e15e2f9723fd9a08
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-11-02 22:23:10 -0700

    removed old theme

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -7,32 +7,3 @@ const config: ThemeConfig = {
   }
   
 export const chakraTheme = extendTheme({config});
-
-// //TODO: Remove Old theme
-// export const theme = {
-//     palette: {
-//         type: 'light',
-//         primary: {
-//           main: '#009688',
-//           light: '#b2dfdb',
-//           dark: '#00796b',
-//         },
-//         secondary: {
-//           main: '#ff9800',
-//           light: '#ffcc80',
-//           dark: '#f57c00',
-//         },
-//       },
-// }
-
-// type CustomTheme = {
-//     [Key in keyof typeof theme]: typeof theme[Key]
-// }
-
-// declare module '@mui/material/styles' {
-//     interface Theme extends CustomTheme { }
-//     interface ThemeOptions extends CustomTheme { }
-// }
-
-// export default createTheme(theme)
-

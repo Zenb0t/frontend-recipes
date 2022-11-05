e1d18e6519e6a4516b36019740f01eb2c89e2d43
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-11-05 16:23:18 -0700

    split into smaller components

--- src/pages/ingredient-page.tsx
+++ src/pages/ingredient-page.tsx
@@ -1,134 +1,53 @@
-import { Box, Button, FormControl, FormErrorMessage, FormLabel, Icon, Input, Tooltip, useColorModeValue, VStack } from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
+import { Box,  Input, InputGroup, InputLeftElement, useColorModeValue } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
+import { MdSearch } from "react-icons/md";
+import { useAppSelector } from "../app/hooks";
 import { IngredientListTable } from "../components/ingredient-table";
-import { SearchIngredient } from "../components/search-ingredient";
-import { demoList } from "../services/fake-data";
-import { yup } from '../app/utils';
-import { FaRegQuestionCircle } from "react-icons/fa";
-import { IngredientItem, IngredientModel } from "../features/recipeBook/models";
-import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { createIngredient, selectIngredientList } from "../features/recipeBook/ingredient-slice";
+
+import { selectIngredientList } from "../features/recipeBook/ingredient-slice";
 
 export function IngredientPage() {
 
     const storeIngredients = useAppSelector(selectIngredientList);
+
+    //State
+    const [searchValue, setSeachValue] = useState("");
+    const searchResults = searchValue === ""
+        ? storeIngredients
+        : storeIngredients.filter(
+            (ing) => {
+                //filter by all fields
+                return ing.name.toLowerCase().includes(searchValue.toLowerCase())
+                    || ing.amount.toString().toLowerCase().includes(searchValue.toLowerCase())
+                    || ing.measuringUnit.toLowerCase().includes(searchValue.toLowerCase())
+                    || ing.cost.toString().toLowerCase().includes(searchValue.toLowerCase())
+            });
+    // Handlers
+
+    const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+        setSeachValue(event.target.value);
+    };
 
     return (
         <Box
             shadow="md"
             bg={useColorModeValue("white", "gray.800")}
             borderRadius="lg">
-            <Box bg={useColorModeValue("white", "gray.800")} p={6} rounded="md" minW={{ base: 200, sm: 300, md: 440, lg: 700 }} mx={"auto"}>
-                <IngredientListTable ingredients={storeIngredients} />
+            <Box p={4}>
+                <InputGroup size="md">
+                    <InputLeftElement pointerEvents="none" children={<MdSearch />} />
+                    <Input
+                        type="text"
+                        placeholder="Search Ingredients"
+                        value={searchValue}
+                        onChange={handleSearch}
+                        variant="filled"
+                    />
+                </InputGroup>
             </Box>
-            <SearchIngredient list={storeIngredients} />
-        </Box>
-    );
-}
-
-
-export function AddIngredientForm({ onClose: handleClose}: { onClose: () => void }) {
-    
-    const dispatch = useAppDispatch();
-
-    interface Values {
-        name: string;
-        amount: number;
-        measuringUnit: string;
-        cost: number;
-        unitCost: number;
-        id: string;
-    }
-
-    const initialValues: Values = {
-        name: "",
-        amount: 0,
-        measuringUnit: "",
-        cost: 0,
-        unitCost: 0,
-        id: ""
-    };
-
-    const ingredientValidation = yup.object().shape({
-        name: yup.string().required("Name is required"),
-        amount: yup.number().required("Amount is required").moreThan(0, "Amount must be greater than 0"),
-        measuringUnit: yup.string().required("Measuring unit is required"),
-        cost: yup.number().required("Cost is required").moreThan(0, "Amount must be greater than 0"),
-    });
-
-    return (
-        <Box bg={useColorModeValue("white", "gray.800")}>
-            <Formik
-                initialValues={initialValues}
-                validationSchema={ingredientValidation}
-                onSubmit={(values) => {
-                    values.unitCost = values.cost / values.amount;
-                    const newIngredient: IngredientModel = {...values}
-                    dispatch(createIngredient(newIngredient));
-                    handleClose();
-                }}
-            >
-                {({ handleSubmit, errors, touched }) => (
-                    <VStack spacing={4} align="center">
-                        <Form onSubmit={handleSubmit}>
-                            <FormControl isInvalid={!!errors.name && touched.name}>
-                                <FormLabel htmlFor="name">Ingredient Name</FormLabel>
-                                <Field
-                                    as={Input}
-                                    id="name"
-                                    name="name"
-                                    type="name"
-                                    variant="filled"
-                                />
-                                <FormErrorMessage>{errors.name}</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={!!errors.amount && touched.amount}>
-                                <Tooltip placement="top" label="Amount of ingredient in the package">
-                                    <FormLabel htmlFor="amount">
-                                        Amount
-                                        <Icon ml={2} as={FaRegQuestionCircle} />
-                                    </FormLabel>
-                                </Tooltip>
-                                <Field
-                                    as={Input}
-                                    id="amount"
-                                    name="amount"
-                                    type="amount"
-                                    variant="filled"
-                                    textAlign={"right"}
-                                />
-                                <FormErrorMessage>{errors.amount}</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={!!errors.measuringUnit && touched.measuringUnit}>
-                                <FormLabel htmlFor="measuringUnit">Measuring Unit</FormLabel>
-                                <Field
-                                    as={Input}
-                                    id="measuringUnit"
-                                    name="measuringUnit"
-                                    type="measuringUnit"
-                                    variant="filled"
-                                />
-                                <FormErrorMessage>{errors.measuringUnit}</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={!!errors.cost && touched.cost}>
-                                <FormLabel htmlFor="cost">Cost</FormLabel>
-                                <Field
-                                    as={Input}
-                                    id="cost"
-                                    name="cost"
-                                    type="cost"
-                                    variant="filled"
-                                    textAlign={"right"}
-                                />
-                                <FormErrorMessage>{errors.cost}</FormErrorMessage>
-                            </FormControl>
-                            <Button type="submit" colorScheme="green" width="full">
-                                Submit
-                            </Button>
-                        </Form>
-                    </VStack>
-                )}
-            </Formik>
+            <Box bg={useColorModeValue("white", "gray.800")} p={6} rounded="md" minW={{ base: 200, sm: 300, md: 440, lg: 700 }} mx={"auto"}>
+                <IngredientListTable ingredients={searchResults} />
+            </Box>
         </Box>
     );
 }

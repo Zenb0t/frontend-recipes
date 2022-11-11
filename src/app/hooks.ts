c222fb879d12a80bd433ca5f9cf01bf3bf3122aa
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-11-11 12:20:13 -0800

    added new useIngredientList hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,12 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { selectIngredientList } from '../features/recipeBook/ingredient-slice';
 import type { RootState, AppDispatch } from './store';
 
 // React-Redux hooks
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Selector hooks
+export function useIngredientList() {
+    return useAppSelector(selectIngredientList);
+}

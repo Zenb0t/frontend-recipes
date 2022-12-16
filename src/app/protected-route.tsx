abb616eb1886c7c3e44f506c4d99a273d87de61e
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-12-15 21:51:47 -0800

    Fetching data only if authenticated.

--- src/app/protected-route.tsx
+++ src/app/protected-route.tsx
@@ -1,12 +1,36 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { DashboardLayout } from "../components/dashboard-layout";
+import { fetchIngredients } from "../features/recipeBook/ingredient-slice";
+import { fetchRecipes } from "../features/recipeBook/recipe-slice";
+import { useAppDispatch, useAppSelector } from "./hooks";
 
 function ProtectedRoute() {
     const { isAuthenticated } = useAuth0();
 
+    const dispatch = useAppDispatch();
+
+    const recipeStatus = useAppSelector(state => state.recipeBook.status);
+    const ingredientStatus = useAppSelector(state => state.ingredients.status);
+
+    //Fetch recipes on mount is the user is authenticated
+    useEffect(() => {
+        if (isAuthenticated) {
+            if (recipeStatus === 'idle') {
+                dispatch(fetchRecipes());
+            }
+            if (ingredientStatus === 'idle') {
+                dispatch(fetchIngredients());
+            }
+        }
+    }, [recipeStatus, ingredientStatus, dispatch, isAuthenticated]);
+
+
     if (isAuthenticated) {
+
+
+
         return <DashboardLayout />;
     } else {
         return <Navigate to="/login" />;

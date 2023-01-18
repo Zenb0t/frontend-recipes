57191d6212b077e990eddd5ed235080d72808406
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-01-17 22:36:49 -0800

    Refactor: Query Params and new HTTP code

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,113 +14,116 @@ export default function apiService() {
 
     const currentUser = store.getState().users.userInfo;
     const token = store.getState().users.userToken;
-    
-            const http = axios.create({
-                baseURL: sanitizedConfig.API_URL,
-                timeout: 1000,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`,
-                }
-            });
 
-            // User API calls
-
-            /**
-             * Send the current User from auth service to the backend. If the user already exists, the backend will return that user's UserModel, 
-             * else it will create a new user and return it.
-             */
-            const sendUser =async (user: User) => {
-                const res = await http.post(`api/u/`, user);
-                return res;
-            }
-
-            const getUser = (id: string) => {
-                return http.get(`api/u/${id}`);
-            }
-
-            const getUserByEmail = (email: string) => {
-                return http.get(`api/u/${email}`);
-            }
-
-            // Recipe API calls
-
-            const createRecipe = async (recipe: RecipeModel) => {
-                const res = await http.post(`api/u/recipes`, recipe);
-                return res;
-            }
-
-            const getRecipes = async () => {
-                const res = await http.get(`api/u/recipes`);
-                return res;
-            }
-
-            const getRecipesByUser = async (userId: string) => {
-                const res = await http.get(`api/u/${userId}/recipes`);
-                return res;
-            }      
-            
-            const getRecipesByUserEmail = async (email: string) => {
-                const res = await http.get(`api/u/${email}/recipes`);
-                return res;
-            } 
-
-            const getRecipe = (id: string) => {
-                return http.get(`api/u/recipes/${id}`);
-            }
-
-            const updateRecipe = (id: string, recipe: RecipeModel) => {
-                return http.put(`api/u/recipes/${id}`, recipe);
-            }
-
-            const deleteRecipe = (id: string) => {
-                return http.delete(`api/recipes/${id}`);
-            }
-
-            const deleteAllRecipes = () => {
-                return http.delete(`api/recipes`);
-            }
-
-            // Ingredient API calls
-
-            const createIngredient = async (ingredient: IngredientModel) => {
-                const res = await http.post(`api/u/ingredients`, ingredient);
-                return res;
-            }
-
-            const fetchIngredients = async () => {
-                const res = await http.get(`api/u/ingredients`);
-                return res;
-            }
-
-            const fetchIngredient = (id: string) => {
-                return http.get(`api/u/ingredients/${id}`);
-            }
-
-            const updateIngredient = (id: string, ingredient: IngredientModel) => {
-                return http.put(`api/u/ingredients/${id}`, ingredient);
-            }
-
-            const deleteIngredient = (id: string) => {
-                return http.delete(`api/u/ingredients/${id}`);
-            }
-
-            return {
-                sendUser,
-                getUser,
-                getUserByEmail,
-                createRecipe,
-                getRecipes,
-                getRecipe,
-                getRecipesByUserEmail,
-                updateRecipe,
-                deleteRecipe,
-                deleteAllRecipes,
-                createIngredient,
-                fetchIngredients,
-                fetchIngredient,
-                updateIngredient,
-                deleteIngredient,
-                getRecipesByUser
-            }
+    const http = axios.create({
+        baseURL: sanitizedConfig.API_URL,
+        timeout: 1000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
         }
+    });
+
+    // User API calls
+
+    /**
+     * Send the current User from auth service to the backend. If the user already exists, the backend will 
+     * return 200 OK and the user information from the database, else it will create a new user and return it.
+     */
+    const sendUser = async (user: User) => {
+        const res = await http.post(`api/u/`, user);
+        return res;
+    }
+
+    /**
+     * Get the current user from the backend, using id as query paramenter.
+     */ 
+    const getUser = (id: string) => {
+        return http.get(`api/u/`,{ params: { userId: id }});
+    }
+
+    const getUserByEmail = (email: string) => {
+        return http.get(`api/u/`, { params: { email: email } });
+    }
+
+    // Recipe API calls
+
+    const createRecipe = async (recipe: RecipeModel) => {
+        const res = await http.post(`api/u/recipes`, recipe, {params: {userId: currentUser!.id}});
+        return res;
+    }
+
+    const getRecipes = async () => {
+        const res = await http.get(`api/u/recipes`, );
+        return res;
+    }
+
+    const getRecipesByUser = async (userId: string) => {
+        const res = await http.get(`api/u/recipes/all`, { params: { userId: userId} });
+        return res;
+    }
+
+    const getRecipesByUserEmail = async (email: string) => {
+        const res = await http.get(`api/u/recipes`, { params: { email: email } });
+        return res;
+    }
+
+    const getRecipe = (id: string) => {
+        return http.get(`api/u/recipes/${id}`);
+    }
+
+    const updateRecipe = (id: string, recipe: RecipeModel) => {
+        return http.put(`api/u/recipes/${id}`, recipe);
+    }
+
+    const deleteRecipe = (id: string) => {
+        return http.delete(`api/recipes/${id}`);
+    }
+
+    const deleteAllRecipes = () => {
+        return http.delete(`api/recipes`);
+    }
+
+    // Ingredient API calls
+
+    const createIngredient = async (ingredient: IngredientModel) => {
+        const res = await http.post(`api/u/ingredients`, ingredient);
+        return res;
+    }
+
+    const fetchIngredients = async () => {
+        const res = await http.get(`api/u/ingredients`);
+        return res;
+    }
+
+    const fetchIngredient = (id: string) => {
+        return http.get(`api/u/ingredients/${id}`);
+    }
+
+    const updateIngredient = (id: string, ingredient: IngredientModel) => {
+        return http.put(`api/u/ingredients/${id}`, ingredient);
+    }
+
+    const deleteIngredient = (id: string) => {
+        return http.delete(`api/u/ingredients/${id}`);
+    }
+
+    return {
+        sendUser,
+        getUser,
+        getUserByEmail,
+        createRecipe,
+        getRecipes,
+        getRecipe,
+        getRecipesByUserEmail,
+        updateRecipe,
+        deleteRecipe,
+        deleteAllRecipes,
+        createIngredient,
+        fetchIngredients,
+        fetchIngredient,
+        updateIngredient,
+        deleteIngredient,
+        getRecipesByUser
+    }
+}

f45c9f55e699994a1f703d410a1a540491dfb536
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-03-02 22:50:31 -0800

    Removed unused dep

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './app/app-router';
 import { chakraTheme } from './app/theme';
@@ -37,8 +36,3 @@ root.render(
     </Provider>
   </StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

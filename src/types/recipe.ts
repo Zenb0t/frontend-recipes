726e8db0689187a4cb1d74158512022ea1530670
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-12-06 21:51:53 -0800

    Add recipe validation schema

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,4 +1,5 @@
 import { Ingredient } from "./ingredient";
+import * as Yup from "yup";
 
 export interface Recipe {
 	_id?: string;
@@ -18,3 +19,25 @@ export interface IngredientItem {
 	quantity: number;
 	measuringUnit: string;
 }
+
+export const recipeValidationSchema = Yup.object({
+	title: Yup.string().required("Required"),
+	description: Yup.string().required("Required"),
+	totalTimeInMinutes: Yup.number()
+	  .required("Required")
+	  .positive("Must be positive"),
+	ingredients: Yup.array().of(
+	  Yup.object({
+		ingredient: Yup.object({
+		  name: Yup.string().required("Required"),
+		  measuringUnit: Yup.string().required("Required"),
+		  amount: Yup.number().required("Required").positive("Must be positive"),
+		}),
+		quantity: Yup.number().required("Required").positive("Must be positive"),
+		measuringUnit: Yup.string().required("Required"),
+	  })
+	),
+	instructions: Yup.array().of(Yup.string().required("Required")),
+	imageUrl: Yup.string().required("Required"),
+	sourceUrl: Yup.string().url("Must be a valid URL"),
+  });
